10ee26b220e46574c05ce2a64c83a688d6143d52
RAVALI28 <ravalikavajinapally.rv@gmail.com>
2025-04-17 15:45:46 -0500

    Intial Commit

---
+++ Springboot-app-with-Kafka/src/main/java/com/DemoSpringbootAppWithKafka/Springboot/app/with/Kafka/SpringbootAppWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.DemoSpringbootAppWithKafka.Springboot.app.with.Kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppWithKafkaApplication.class, args);
+	}
+
+}
---
+++ Springboot-app-with-Kafka/src/test/java/com/DemoSpringbootAppWithKafka/Springboot/app/with/Kafka/SpringbootAppWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DemoSpringbootAppWithKafka.Springboot.app.with.Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
